f1ea2079687932b7d0f72332a0f0ab1f44bbc80c
Chine Anikwe <c.anikwe@gmail.com>
2018-11-04 18:27:57 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,10 +34,9 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  valid_move?(board, index)
-      while !valid_move?(board, index)
-        puts "Please try again"
-        input = gets.strip
-        index = input_to_index(input)
-      end
+  while !valid_move?(board, index)
+    puts "Please try again"
+    input = gets.strip
+    index = input_to_index(input)
+  end
 end
