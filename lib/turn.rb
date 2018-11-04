bbe732bec521e2a787c8b717b1a41bffda4c6085
Chine Anikwe <c.anikwe@gmail.com>
2018-11-04 18:24:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,10 +31,9 @@ def move(board, index, token = "X")
 end
 
 def turn(board)
-  while !valid_move?(board, index)
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
     valid_move?(board, index)
-  end
+  
 end
