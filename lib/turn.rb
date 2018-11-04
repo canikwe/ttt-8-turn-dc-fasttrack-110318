b8a44bcc88af880032356046a4274f696116e08b
Chine Anikwe <c.anikwe@gmail.com>
2018-11-04 18:15:18 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,35 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == " "
+    return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+  return index < 9 && index >= 0 && !position_taken?(board, index)
+end
+
+def move(board, index, token = "X")
+    board[index] = token
+end
+
+def turn
+  puts "Please enter 1-9:"
+end
