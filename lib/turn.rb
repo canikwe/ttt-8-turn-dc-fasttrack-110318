f6fb9fcfa8c3a5c303a14473a570a785e7c8c3d4
Chine Anikwe <c.anikwe@gmail.com>
2018-11-04 19:25:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,7 +39,7 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
   end
-  if valid_move(board, index)
+  if valid_move(board, index) == true
     move(board, index, "X")
   end
 end
